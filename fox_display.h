82345c1367230fa6341e3c07e8af5901efd2bcaa
yudhaime <36374524+yudhaime@users.noreply.github.com>
2026-01-16 13:50:10 +0700

    Add new display functions and include fox_config.h

--- fox_display.h
+++ fox_display.h
@@ -2,20 +2,25 @@
 #define FOX_DISPLAY_H
 
 #include <Arduino.h>
+#include "fox_config.h"  // HARUS INCLUDE untuk PAGE_x_ENABLED
 
 // Forward declaration saja
 struct FoxVehicleData;
 
+// Fungsi publik utama
 void foxDisplayInit();
 void foxDisplayUpdate(int page);
 void foxDisplayShowSetupMode(bool blinkState);
 void foxDisplayForceSportUpdate();
 bool foxDisplayIsInitialized();
 
-// Fungsi internal - gunakan forward declaration
+// Fungsi internal untuk tiap page
 void displayPageClock();
 void displayPageTemperature(const FoxVehicleData& vehicleData);
+void displayPageElectrical(const FoxVehicleData& vehicleData); // SELALU DEKLARASI
 void displayPageSport(const FoxVehicleData& vehicleData);
+
+// Fungsi helper untuk sport page
 void updateBlinkState();
 void displayCruiseMode();
 void displaySportModeLowSpeed();
