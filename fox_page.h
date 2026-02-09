24260ab919f5420c1c166f55c0c4706e7c726bdf
yudhaime <36374524+yudhaime@users.noreply.github.com>
2026-02-09 07:56:44 +0700

    MANAGEMEN URUTAN PAGE
    Removed unused mode types and functions, added page order management.

--- fox_page.h
+++ fox_page.h
@@ -3,52 +3,35 @@
 
 #include <Arduino.h>
 
-// =============================================
-// PAGE & MODE MANAGEMENT (TANPA CHARGING)
-// =============================================
-
-// Mode types - HANYA SPORT & CRUISE
+// Mode types - SIMPLIFIED
 typedef enum {
     MODE_NORMAL = 0,
-    MODE_SPORT = 1,
-    MODE_CRUISE = 2,
-    // MODE_CHARGING = 3  // DIHAPUS
 } SpecialMode;
 
 // Global variables
 extern int currentPage;
 extern bool setupMode;
-extern bool pageLocked;
-extern bool wasCharging; // Masih ada untuk kompatibilitas
 extern uint8_t currentSpecialMode;
-extern int lastNormalPage;
 
-// Button handling
-extern bool lastButton;
-extern unsigned long setupModeStart;
-extern unsigned long lastButtonPress;
+// NEW: Page order extern declarations
+extern const uint8_t PAGE_ORDER[];
+extern const uint8_t PAGE_ORDER_COUNT;
 
 // Function declarations
 void initButton();
 bool checkButtonPress();
 void handleButtonPress();
 void updateModeDetection();
-void handleSpecialModeDisplay();
-void returnToNormalMode();
 
 // Page management
 void switchToPage(int page);
-void switchToSpecialMode(SpecialMode mode);
-void forceNormalMode();
+
+// NEW: Page order functions
+int getNextPageInOrder(int currentPage);
+int getFirstEnabledPage();
 
 // Getter functions
 uint8_t getCurrentSpecialMode();
-bool isInSpecialMode();
 int getCurrentDisplayPage();
-int getLastNormalDisplayPage();
-
-// NEW FUNCTIONS
-void enterSpecialMode(uint8_t mode);
-void handleCruiseBlink();
 
 #endif
