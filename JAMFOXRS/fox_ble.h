40d2bd2dc6d1294bbc03ae700d0f23b4df849ebe
yudhaime <36374524+yudhaime@users.noreply.github.com>
2026-03-18 10:18:10 +0700

    Reorganize and add BLE function declarations

--- JAMFOXRS/fox_ble.h
+++ JAMFOXRS/fox_ble.h
@@ -2,6 +2,7 @@
 #define FOX_BLE_H
 
 #include <Arduino.h>
+#include "fox_config.h"
 
 // BLE Configuration
 #define BLE_DEVICE_NAME "Votol_BLE"
@@ -9,27 +10,14 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 // BLE Timing
-#define BLE_FAST_MIN_MS 100     
-#define BLE_FAST_MAX_MS 500      
-#define BLE_SLOW_MIN_MS 1000     
-#define BLE_SLOW_MAX_MS 2000     
+#define BLE_FAST_MIN_MS 100
+#define BLE_FAST_MAX_MS 500
+#define BLE_SLOW_MIN_MS 1000
+#define BLE_SLOW_MAX_MS 2000
 
 #define BLE_SAFE_CHUNK 240
 #define BLE_PUMP_BUDGET_US 10000
 #define BLE_PUMP_MAX_CHUNKS 4
-
-// Function declarations
-void initBLE();
-void processBLE();
-void updateBLEData();
-bool isBLEConnected();
-bool isBLEActive();
-bool isInAppMode();  
-void activateBLE();
-void deactivateBLE();
-bool isBLEActivationPending();
-void setBLEActivationPending(bool pending, unsigned long startTime = 0);
-void resetBLEActivation();
 
 // Vehicle mode for adaptive timing
 typedef enum {
@@ -41,5 +29,25 @@ typedef enum {
     MODE_REVERSE,
     MODE_BRAKE
 } VehicleMode;
+
+// Function declarations
+void activateBLE();
+void deactivateBLE();
+void processBLE();
+void updateBLEData();
+bool isBLEConnected();
+bool isBLEActive();
+bool isInAppMode();
+bool isBLEActivationPending();
+void setBLEActivationPending(bool pending, unsigned long startTime = 0);
+void resetBLEActivation();
+void printBLEStatus();
+
+// Display functions for BLE
+void showAppModeDisplay();
+void showBleOffDisplay();
+
+// External variable for display
+extern volatile bool deviceConnected;
 
 #endif
